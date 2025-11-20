918f96a82a48a03f2dbc6e5c177810d7a500ba0a
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-20 14:28:01 +0900

    ステップ7：taskページ一覧表示・新規作成・修正・削除　実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,50 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order(updated_at: :desc)
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "タスクが作成されました"
+      redirect_to tasks_path, status: :see_other
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "タスクが更新されました"
+      redirect_to tasks_path, status: :see_other
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = "タスクが削除されました"
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
