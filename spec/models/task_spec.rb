f22e9facda4529891f00740a549c0214d2d5e9b7
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-21 17:17:27 +0900

    ステップ9：テストコード実装

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+
+    it "タイトルは必須であること" do
+      task = Task.new(title: nil, content: "内容")
+      expect(task).not_to be_valid
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+    it "タスク内容は500文字以内なら有効" do
+      task = Task.new(title: "title", content: "a" * 500)
+      expect(task).to be_valid
+    end
+    it "タスク内容は501文字以上なら無効" do
+      task = Task.new(title: "title", content: "a" * 501)
+      expect(task).not_to be_valid
+      expect(task.errors[:content]).to include("is too long (maximum is 500 characters)")
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,12 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
