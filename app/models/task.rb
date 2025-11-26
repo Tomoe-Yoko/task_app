1f7bea2b19529d2d691bba3535e0c3b30bf0045e
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-26 22:34:01 +0900

    終了期限の追加実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,12 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, length: { maximum: 500 }
+  validates :deadline, presence: true
   validate :deadline_cannot_be_in_the_past
 
   def deadline_cannot_be_in_the_past
-    return if deadline.present? && deadline < Time.zone.today
+    return if deadline.blank?
+    return if deadline >= Time.zone.today
 
     errors.add(:deadline, "締め切り日は今日以降の日付にしてください")
   end
