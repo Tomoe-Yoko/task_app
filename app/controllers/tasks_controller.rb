0758cbe81d8d9cdb4ce8552351de260c1883acbe
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-27 09:14:36 +0900

    終了期限タブのパーシャル化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,10 +4,10 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.order(updated_at: :desc)
 
-    if params[:latest]
-      @tasks = Task.latest
-    elsif params[:updated]
+    if params[:updated]
       @tasks = Task.updated
+    elsif params[:latest]
+      @tasks = Task.latest
     elsif params[:created]
       @tasks = Task.created
     end
