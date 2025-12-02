ebccabf5f8a314abfc0320826548b5e5d87e47da
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-12-02 09:31:14 +0900

    Lint,Testエラー修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
- 
+
   def index
     @search_form = TaskSearchForm.new(search_params)
     @tasks = @search_form.search
