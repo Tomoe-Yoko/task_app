abdd97ced1f2d5b7d03bd005aa37dcb0ad6aafba
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-12-04 15:35:17 +0900

    :authenticate_user!をapprication.controllerに入れる重要性を理解

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pagy::Method # pagenation
 
+  before_action :authenticate_user!
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path # ← ログイン画面に戻す
   end
