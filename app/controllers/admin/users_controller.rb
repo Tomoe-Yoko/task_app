77cfb5e6675658e52ebb3d43e67168fa5f51a245
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-12-05 11:46:09 +0900

    名前空間をネスト構造で定義修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,13 @@
-class Admin::UsersController < ApplicationController
-  def index
-    @users = User.includes(:tasks) # N+1問題対策
-    p @users
-  end
+module Admin
+  class UsersController < ApplicationController
+    def index
+      @users = User.includes(:tasks) # N+1問題対策
+    end
 
-  private
+    private
 
-  def search_params
-    params.fetch(:task_search_form, {}).permit(:title, :status, :order, :priority)
+    def search_params
+      params.fetch(:task_search_form, {}).permit(:title, :status, :order, :priority)
+    end
   end
 end
